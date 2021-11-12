f80b14178af8e5d174f6a643115d75e6a7e6cb5f
taj1-takum1 <takumin390511@gmail.com>
2021-11-12 13:34:43 +0900

    削除機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: %i[show edti update]
+
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -17,20 +19,32 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(params[:id])
+    else
+      render :edit
+    end
+  end
 
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
   end
 
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
