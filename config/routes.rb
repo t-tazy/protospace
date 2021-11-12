f632f71480a34d0caa957bcd78ad0ea64ea076bd
taj1-takum1 <takumin390511@gmail.com>
2021-11-12 15:42:11 +0900

    コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: %i[new create show edit update destroy]
+  resources :prototypes do
+    resources :comments, only: :create
+  end
 end
